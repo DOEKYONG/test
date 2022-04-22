d5cb16d108bc577488099f74945a2fc2e1c97ded
504 <504@DESKTOP-2M11V1C>
2022-04-22 14:19:46 +0900

    새로운브랜치 커밋

--- src/main/java/평가/UserDao.java
+++ src/main/java/평가/UserDao.java
@@ -7,22 +7,29 @@ import java.sql.ResultSet;
 
 
 public class UserDao {
+
 	
-	private Connection con; // DB연동시 사용되는 클래스 : DB연동클래스
-	private PreparedStatement ps; // 연결된 DB내 SQL 조작 할때 사용되는 인터페이스 : DB조작인터페이스
-	private ResultSet rs; // 결과물을 조작하는 인터페이스 
+	private static Connection con; // DB연동시 사용되는 클래스 : DB연동클래스
+	private  static PreparedStatement ps; // 연결된 DB내 SQL 조작 할때 사용되는 인터페이스 : DB조작인터페이스
+	private static ResultSet rs; // 결과물을 조작하는 인터페이스 
 	
 	public static UserDao userDao = new UserDao(); // DB 연동 객체;
 	
-	public UserDao() {
+	
+	
+	
+	public static void main(String[] args) {
 		try {
 			// DB연동 
 			Class.forName("com.mysql.cj.jdbc.Driver"); // 1. DB 드라이버 가져오기
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/javafx?serverTimezone=UTC",
 					"root","1234"); // 2. DB 주소 연결 
+			System.out.println("연동성공");
 		}
 		catch(Exception e ) { System.out.println( "[DB 연동 오류]"+e  ); }
 	}
+	
+	}
 
-}
+
 //
